a3b2478d9e856d1b0d16eb8ab35694e47cf7b12e
Andre Mendes <andremendesmarcondes@gmail.com>
2021-08-02 21:56:10 -0300

    criando domain com cliente, adicionando referencias ao entityframework e mapeando domain como dbset

---
+++ AR.Data/ContextoPrincipal.cs
@@ -0,0 +1,15 @@
+﻿using AR.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace AR.Data
+{
+    public class ContextoPrincipal : DbContext
+    {
+        public ContextoPrincipal(DbContextOptions options) 
+            : base(options)
+        {
+        }
+
+        public DbSet<Cliente> Cliente { get; set; }
+    }
+}
