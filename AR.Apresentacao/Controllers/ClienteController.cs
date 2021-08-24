737db78c64484278f3103753e95be6a2f9d8dee0
Andre Mendes <andremendesmarcondes@gmail.com>
2021-08-24 09:59:16 -0300

    criando controller cliente, adicionando migration tabela cliente e fazendo injeções

---
+++ AR.Apresentacao/Controllers/ClienteController.cs
@@ -0,0 +1,40 @@
+﻿using AR.Data.Interfaces;
+using AR.Domain;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace AR.Apresentacao.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClienteController : ControllerBase
+    {
+        private readonly IClienteRepository _repository;
+
+        public ClienteController(IClienteRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_repository.GetAll());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(Cliente cliente)
+        {
+            try
+            {
+                await _repository.Add(cliente);
+                return StatusCode(201);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+    }
+}
---
+++ AR.Data/Migrations/20210824125131_Table_Cliente.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AR.Data.Migrations
+{
+    public partial class Table_Cliente : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cliente",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Nome = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    CPF = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Idade = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cliente", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cliente");
+        }
+    }
+}
